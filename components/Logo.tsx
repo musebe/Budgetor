daff07bd8c39c30c28d1abb6d279876c79fd94b7
musebe <emusebe9@gmail.com>
2024-05-03 17:25:45 +0300

    feat: initialize Prisma schema and configure database setup

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,4 @@
-import { HandCoinsIcon, PiggyBank } from 'lucide-react'
+import { HandCoinsIcon} from 'lucide-react'
 import React from 'react'
 
 function Logo() {
@@ -16,6 +16,9 @@ export function LogoMobile() {
   return (
     <a href='/' className='flex items-center gap-2'>
       <HandCoinsIcon className='stroke h-11 w-11 stroke-amber-500 stroke-[1.5]' />
+      <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-2xl font-bold leading-tight tracking-tighter text-transparent'>
+        Budgetor
+      </p>
     </a>
   );
 }
